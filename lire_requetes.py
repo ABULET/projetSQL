06c3db4f8af04611f2445533d130d270f5b29431
ABULET <78963637+ABULET@users.noreply.github.com>
2021-02-19 14:28:58 +0100

    je n'arrive pas à mettre les requêtes

---
+++ lire_requetes.py
@@ -0,0 +1,8 @@
+﻿def lire_requetes(dico,fichier):
+    with open(fichier,"r") as requete:
+        lignes = filin.readlignes()
+        for ligne in lignes:
+            dico.append(ligne)
+    return dico
+
+lire_requetes({},"req1.txt")
