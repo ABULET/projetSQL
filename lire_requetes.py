416c024d39a54a80ec588cd96798cf97bcb521cc
ABULET <78963637+ABULET@users.noreply.github.com>
2021-03-08 08:40:37 +0100

    Delete lire_requetes.py

--- lire_requetes.py
+++
@@ -1,8 +0,0 @@
-﻿def lire_requetes(dico,fichier):
-    with open(fichier,"r") as requete:
-        lignes = filin.readlignes()
-        for ligne in lignes:
-            dico.append(ligne)
-    return dico
-
-lire_requetes({},"req1.txt")
